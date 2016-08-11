f5af6aa7772768cdd0e2a702be05b29d33ddb068
Vetriveld <vetriveldcse@gmail.com>
2016-08-11 17:18:52 +0530

    Create check.java

---
+++ check.java
@@ -0,0 +1,14 @@
+class Demo
+{
+public static void main(String []args)
+{
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+if(a<0)
+System.out.println("negative");
+else if(a>0)
+System.out.println("POSITIVE");
+else
+System.out.println("zero");
+}
+}
